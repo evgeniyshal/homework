94bc36edb0d2a4450ef7376ce5be65674540d922
acidburn <evgeniyshal@gmai.com>
2021-03-16 10:51:18 +0300

    добавил TASKS

--- src/homeWork/Task3.java
+++ src/homeWork/Task3.java
@@ -1,4 +1,4 @@
-package HomeWork;
+package homeWork;
 
 public class Task3 {
     public static void main(String[] args) {
--- src/homeWork/Task4.java
+++ src/homeWork/Task4.java
@@ -1,4 +1,4 @@
-package HomeWork;
+package homeWork;
 
 public class Task4 {
     public static void main(String[] args) {
