ab91df54435f50ed8f9a56173839fa17297f6cdc
acidburn <evgeniyshal@gmai.com>
2021-03-19 13:05:29 +0300

    добавил TASKS

--- src/homeworkLesson2/Task4.java
+++ src/homeworkLesson2/Task4.java
@@ -6,7 +6,7 @@ public class Task4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int randomNum = (int) ((Math.random() * 8)+1);
+        int randomNum = (int) ((Math.random() * 8) + 1);
         System.out.println("Введите число:");
 
         while (true) {
