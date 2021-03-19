dff8d784dda302b1307b9fe873fbca2e5d33f06e
acidburn <evgeniyshal@gmai.com>
2021-03-19 12:49:25 +0300

    добавил TASKS

--- src/homeworkLesson2/Task4.java
+++ src/homeworkLesson2/Task4.java
@@ -1,4 +1,4 @@
-package homeworkLesson2;
+ 4package homeworkLesson2;
 
 import java.util.Scanner;
 
@@ -6,22 +6,22 @@ public class Task4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int randomNum = (int) (Math.random()*8+1);
+        int randomNum = (int) ((Math.random() * 9));
 
-        for (int i = 1; i < 10; i++) {
-            System.out.println("Введите число:");
-            int userNum = sc.nextInt();
 
+        System.out.println("Введите число:");
+        int userNum = sc.nextInt();
+        while (true) {
             if (userNum == 0) {
                 System.out.println("Выход из программы");
                 break;
-            } else if (userNum == randomNum) {
-                    System.out.println("Вы угадали!");
-                    break;
-            } else if ( userNum < randomNum) {
+            } if (userNum == randomNum) {
+                System.out.println("Вы угадали!");
+                break;
+            } if (userNum < randomNum) {
                 System.out.println("загаданное число меньше");
-            } else if ( userNum > randomNum) {
-                    System.out.println("загаданное число больше");
+            } if (userNum > randomNum) {
+                System.out.println("загаданное число больше");
             }
 
         }
