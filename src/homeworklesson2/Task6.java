2c27007197bf343f79694002cf405b9527963f45
evgen <evgeniyshal@gmail.com>
2021-03-20 18:28:02 +0300

    добавил TASKS lesson2

--- src/homeworklesson2/Task6.java
+++ src/homeworklesson2/Task6.java
@@ -7,22 +7,25 @@ public class Task6 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Загадать число от 1 до 100!");
-        int minNumber = 1;
-        int maxNumber = 100;
+        int minNum = 1;
+        int maxNum = 100;
 
-        System.out.println("число больше 49?");
-        System.out.println(" Да - 1, Нет 2");
-        int userNumber = sc.nextInt();
 
-        if (userNumber == 1) {
-            maxNumber = maxNumber /2;
-            System.out.println("Число  ?");
+        while (minNum <= maxNum) {
 
-        } else if (userNumber == 2) {
-            minNumber = minNumber * 25;
-            System.out.println();
+            int midNum = 50;
+            System.out.println("Используйте числа 0 - НЕТ и 1 - ДА");
+            System.out.println("Число " + midNum + " больше загадонного?");
+            int userNumber = sc.nextInt();
 
+
+            if (userNumber == 1) {
+                midNum = ((maxNum + minNum) / 2); // 150 / 2 = 75
+                System.out.println("Число " + midNum + " больше загадонного ?");
+
+            }
         }
-
     }
 }
+
+
