9d542ca2b263f71a64308fc28d54533ff9692030
evgen <evgeniyshal@gmail.com>
2021-04-28 13:37:55 +0300

    hw done

--- src/lesson18/encpryptedIn.java
+++ src/lesson18/encpryptedIn.java
@@ -4,6 +4,16 @@ import java.io.IOException;
 import java.io.InputStream;
 
 class encryptedIn extends InputStream {
+    public static byte[] encrypt(String text, String keyWord){
+        byte[] arr = text.getBytes();
+        byte[] keyArr = keyWord.getBytes();
+        byte[] result = new byte[arr.length];
+        for(int i = 0; i< arr.length; i++) {
+            result[i] = (byte) (arr[i] ^ keyArr[i % keyArr.length]);
+        }
+        return result;
+
+    }
     @Override
     public int read() throws IOException {
         return 0;
--- src/lesson18/encryptedOut.java
+++ src/lesson18/encryptedOut.java
@@ -4,6 +4,14 @@ import java.io.IOException;
 import java.io.OutputStream;
 
 public class encryptedOut extends OutputStream {
+    public static String decrypt(byte[] text, String keyWord) {
+        byte[] result  = new byte[text.length];
+        byte[] keyArr = keyWord.getBytes();
+        for(int i = 0; i < text.length;i++) {
+            result[i] = (byte) (text[i] ^ keyArr[i% keyArr.length]);
+        }
+        return new String(result);
+    }
     @Override
     public void write(int b) throws IOException {
 
